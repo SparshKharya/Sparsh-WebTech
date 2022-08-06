e38d600f2b99314de22c792b8ad420a776539b30
Sparsh Kharya <76450991+SparshKharya@users.noreply.github.com>
2022-08-06 22:41:51 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> JavaScript </h2>
+
+<p id="demo">A Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+</body>
+</html> 
